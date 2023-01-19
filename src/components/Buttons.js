867e66cfda906ceef99946fd6b8734dc6fa5dc8f
Erik Alvarez <erik@velascommerce.com>
2023-01-19 08:06:36 -0400

    Updated send and receive buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
+import PaymentsModal from "./PaymentsModal";
 import "./Buttons.css";
 
 export const Buttons = () => {
+  const [modalState, setModalState] = useState({
+    type: "",
+    open: false,
+  });
+
   return (
     <div>
       <div className="buttons">
         <button
           className="button"
-          onClick={() =>
-            console.log("This button will open a modal to send a payment")
-          }
+          onClick={() => setModalState({ type: "send", open: true })}
         >
           Send
         </button>
         <button
           className="button"
-          onClick={() =>
-            console.log("This button will open a modal to receive a payment")
-          }
+          onClick={() => setModalState({ type: "receive", open: true })}
         >
           Receive
         </button>
       </div>
-      {/* <PaymentsModal modalState={modalState} setModalState={setModalState} /> */}
+      <PaymentsModal modalState={modalState} setModalState={setModalState} />
     </div>
   );
 };
--- src/components/PaymentsModal.js
+++ src/components/PaymentsModal.js
@@ -13,6 +13,23 @@ const customStyles = {
 };
 
 const PaymentsModal = ({ modalState, setModalState }) => {
+  // Our state for the info we will send to either generate a new invoice or pay an invoice
+  const [formData, setFormData] = useState({
+    amount: 0,
+    invoiceToPay: "",
+  });
+  // Our state for storing the invoice we created to be paid
+  const [invoice, setInvoice] = useState("");
+  // Our state for the invoice we paid
+  const [paymentInfo, setPaymentInfo] = useState({
+    paymentHash: "",
+    checkingId: "",
+  });
+
+  const handleSend = () => {};
+
+  const handleReceive = () => {};
+
   return (
     <Modal
       isOpen={modalState.open}
@@ -28,7 +45,55 @@ const PaymentsModal = ({ modalState, setModalState }) => {
       >
         X
       </p>
-      <p>Here's our modal!</p>
+      {/* If it is a send */}
+      {modalState.type === "send" && (
+        <form>
+          <label>paste an invoice</label>
+          <input
+            type="text"
+            value={formData.invoiceToPay}
+            onChange={(e) =>
+              setFormData({ ...formData, invoiceToPay: e.target.value })
+            }
+          />
+          <button className="button" onClick={(e) => handleSend(e)}>
+            Submit
+          </button>
+        </form>
+      )}
+      {/* If it is a receive */}
+      {modalState.type === "receive" && (
+        <form>
+          <label>enter amount</label>
+          <input
+            type="number"
+            min="0"
+            value={formData.amount}
+            onChange={(e) =>
+              setFormData({ ...formData, amount: e.target.value })
+            }
+          />
+          <button className="button" onClick={(e) => handleReceive(e)}>
+            Submit
+          </button>
+        </form>
+      )}
+      {/* If we are displaying our newly created invoice */}
+      {invoice && (
+        <section>
+          <h3>Invoice created</h3>
+          <p>{invoice}</p>
+          {/* ToDo: Create a QR code out of this invoice as well */}
+        </section>
+      )}
+      {/* If we are displaying the status of our successful payment */}
+      {paymentInfo.paymentHash && (
+        <section>
+          <h3>Payment sent</h3>
+          <p>Payment hash: {paymentInfo.paymentHash}</p>
+          <p>Checking id: {paymentInfo.checkingId}</p>
+        </section>
+      )}
     </Modal>
   );
 };
